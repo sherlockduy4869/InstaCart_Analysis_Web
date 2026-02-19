52b7a5a624ee84e6cc245f9064116218a16711e4
Duy-Kun <sherlockduy2001@gmail.com>
2026-02-19 15:07:37 +0100

    Feat: debuggin deployment process

--- InstaCart.py
+++ InstaCart.py
@@ -90,8 +90,8 @@ if submit_button:
     st.subheader('Up to this ???')
     data = data.merge(departments, on = "department_id", how = "left")
     st.subheader('Up to this ????')
-    data = data.merge(orders, on = "order_id", how = "left")
-    st.subheader('Up to this three')
+    # data = data.merge(orders, on = "order_id", how = "left")
+    # st.subheader('Up to this three')
 
     # sns.countplot(x='order_dow', data=data, color='teal', ax=ax)
 
